a5f28cb2b7b3c23c730045e9b707d54bfe24cb89
randon-proctor-kr <randon.proctor@kroger.com>
2022-09-01 13:41:04 -0400

    Added event component lesson

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,11 @@
+const EventComponent: React.FC = () => {
+    const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        console.log(event)
+    }
+
+    return <div>
+        <input onChange={onChange} />
+    </div>
+}
+
+export default EventComponent
