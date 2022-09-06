1daa9e96b113790f0be04691fe80b2984b02e63c
randon-proctor-kr <randon.proctor@kroger.com>
2022-09-06 15:32:45 -0400

    Learned about events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,11 +1,24 @@
+import React from "react"
+
 const EventComponent: React.FC = () => {
-    const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event)
     }
 
-    return <div>
-        <input onChange={onChange} />
-    </div>
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log(`You started dragging at ${event.timeStamp}`)
+    }
+
+    const onDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log(`You stopped dragging at ${event.timeStamp}`)
+    }
+
+    return (
+        <div>
+            <input onChange={onChange} />
+            <div draggable onDragStart={onDragStart} onDragEnd={onDragEnd}>Drag Me!</div>
+        </div>
+    )
 }
 
 export default EventComponent
